08e4552f7405c270be7bd49fe4408356ba2f79d6
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2019-11-17 08:17:52 -0500

    splitting token reading logic

--- src/tea/tokenizer/Tokenizer.java
+++ src/tea/tokenizer/Tokenizer.java
@@ -1,16 +1,31 @@
 package tea.tokenizer;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Tokenizer {
     public static String[] tokenize(String[] lines) {
-        return Stream.of(lines)
+        return new Source(lines).tokens();
+    }
+}
+
+class Source {
+    private final String[] lines;
+    private final String[] sourceLines;
+    private final String[] tokens;
+
+    public Source(String[] lines) {
+        this.lines = lines;
+
+        this.sourceLines = Stream.of(lines)
                 .filter(Predicate.not(String::isBlank))
                 .map(String::trim)
-                .filter(Predicate.not(Tokenizer::isComment))
-                .collect(Collectors.joining(" ")) // assume space at end of lines
+                .filter(Predicate.not(Source::isComment))
+                .toArray(String[]::new);
+
+        this.tokens = String.join(" \n", sourceLines) // assume space at end of lines
                 .replace(";", " ; ") // simplify semicolon tokenization
                 .replace("(", "( ")
                 .replace(")", " ) ")
@@ -23,7 +38,11 @@ public class Tokenizer {
                 .split("\\s");
     }
 
-    private static boolean isComment(String s) {
+    public String[] tokens() {
+        return tokens;
+    }
+
+    static boolean isComment(String s) {
         return s.startsWith("#");
     }
 }
