d82ac4d4f0583de4ed27f915e9e537b9468be61f
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2019-11-22 18:25:03 -0500

    renamed files ... now editors are not confused

--- test/parser/ParseCoreTest.java
+++ test/parser/ParseCoreTest.java
@@ -20,19 +20,19 @@ public class ParseCoreTest {
     @DataProvider
     public static Object[][] readToGraph() throws IOException {
         return new Object[][]{
-                {lines("core/first.c"), "[I`first` I`x` I`y` = V`x`, I`main`  = (V`first` 5 10)]"},
+                {lines("core/first.core"), "[I`first` I`x` I`y` = V`x`, I`main`  = (V`first` 5 10)]"},
         };
     }
 
     @DataProvider
     public static Object[][] compileToStg() throws IOException {
         return new Object[][]{
-                {lines("core/first.c"), "first = {}\\n{x y} -> x {};\nmain = {}\\n{} -> first {5# 10#};\n"},
-                {lines("core/complex_args.c"),
+                {lines("core/first.core"), "first = {}\\n{x y} -> x {};\nmain = {}\\n{} -> first {5# 10#};\n"},
+                {lines("core/complex_args.core"),
                         "sum = {}\\n{x y} -> + {x y};\n" +
                         "sum3_snd = {}\\n{x y z} -> case sum {x y} of {_hid_0 -> sum {_hid_0 z}};\n" +
                         "main = {}\\n{} -> sum3_snd {5# 8# 29#};\n"},
-                {lines("core/curried_args.c"),
+                {lines("core/curried_args.core"),
                         "sum = {}\\n{a b c} -> + {a b c};\n" +
                         "sum3_curried = {}\\n{x y z} -> let {_hid_0 = {x y sum} \\n {} -> sum {x y}} in _hid_0 {z};\n" +
                         "main = {}\\n{} -> sum3_curried {5# 8# 29#};\n"},
